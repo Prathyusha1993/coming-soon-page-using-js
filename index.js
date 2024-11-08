42be117ba14206fd4cc62fb21a13aaa2d62602f8
Prathyusha Kurigala <prathyushakiran@Prathyushas-MBP.attlocal.net>
2024-11-08 15:46:11 -0800

    coming soon page using js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var countDown = new Date('Nov 21, 2024 00:00:00').getTime();
 var x = setInterval(() => {
     var now = new Date().getTime();
     var distance = countDown - now;
-
+    
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
